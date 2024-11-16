3e98c9e08d8ee8b51c341fce212ec031e5482b7b
codeftsaikat <soikatdey18@gmail.com>
2024-11-16 01:00:58 +0100

    Refactor authentication routes and middleware, update NextAuth configuration

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,19 @@
 import NextAuth from "next-auth"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+import Github from 'next-auth/providers/github'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import Credentials from "next-auth/providers/credentials"
+import bcrypt from 'bcryptjs'
+
+export const { handlers: { GET, POST }, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: {strategy: 'jwt'},
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Credentials({
+
+    })
+  ],
 })
