3edde50b6805a49c629fc8daa3df330f61c0f011
codeftsaikat <soikatdey18@gmail.com>
2024-11-16 01:29:26 +0100

    Refactor RootLayout component to include session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { Inter } from 'next/font/google';
+
+import { getSession, SessionProvider } from "next-auth/react";
+
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 
@@ -14,24 +17,30 @@ const inter = Inter({
   style: ['normal', 'italic'],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session = await getSession();
+
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        <div className="flex flex-col min-h-screen">
-          <Header/>
-          <div className="flex-1 flex flex-col">
-            {children}
+    <SessionProvider session={session}>
+
+      <html lang="en">
+        <body
+          className={`${inter.className} antialiased`}
+        >
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <div className="flex-1 flex flex-col">
+              {children}
+            </div>
+            <Footer />
           </div>
-          <Footer/>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
