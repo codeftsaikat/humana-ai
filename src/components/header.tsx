2449e656ad20e79d76949923cbf69e8edd1eb9a9
codeftsaikat <soikatdey18@gmail.com>
2024-11-16 15:06:05 +0100

    Refactor header component and user menu
    - Refactor header component to improve code organization and readability
    - Update user menu component to include logout functionality and profile link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,11 @@
-import { MenuIcon } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
-import Container from "./container"
-import Logo from "./logo"
-import LoginBtn from "./login-btn"
 import { auth } from "@/auth"
-import UserMenu from "./user-menu"
+import Container from "./container"
 import { IAuth } from "@/types/user"
+import Logo from "./logo"
+import UserMenu from "./user-menu"
+import { Button } from "./ui/button"
+import Link from "next/link"
+
 
 const Header = async () => {
 
@@ -21,34 +13,12 @@ const Header = async () => {
 
   return (
     <Container className="max-w-5xl left-1/2 -translate-x-1/2 fixed top-4">
-      <header className="flex justify-between items-center bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border rounded-xl px-4 py-3 shadow-lg">
+      <header className="flex justify-between items-center min-h-16 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border rounded-xl px-4 py-3 shadow-lg">
         <div className="flex items-center gap-2">
           <Logo />
           <h1 className="text-lg font-bold">Humanize</h1>
         </div>
-        <div className="w-fit">
-          {!session ? <LoginBtn /> : <UserMenu auth={session} />}
-        </div>
-        {/* TODO: Configure dropdown */}
-        <div className="block md:hidden">
-          <DropdownMenu modal={false}>
-            <DropdownMenuTrigger asChild>
-              <Button variant='ghost' size='icon'>
-                <MenuIcon />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" align="end">
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  Register
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  Login
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
+        {!session ? <Link href='/login'><Button>Login</Button></Link> : <UserMenu auth={session} />}
       </header>
     </Container>
   )
--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { type FC } from 'react'
 import {
   DropdownMenu,
@@ -9,27 +11,46 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { IAuth } from '@/types/user'
+import Link from 'next/link'
+import { logout } from '@/actions/auth'
 
 interface Props {
   auth: IAuth
 }
 
 const UserMenu: FC<Props> = ({ auth }) => {
+
+  const handleLogout = async () => {
+    await logout();
+  }
+
+  const getInitials = (name: string) => {
+    const [firstName, lastName] = name.split(" ")
+    return firstName.charAt(0) + lastName.charAt(0)
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
+        <Avatar className='shadow-md'>
+          <AvatarImage src={auth.user.image} alt='User avatart' />
+          <AvatarFallback>{getInitials(auth.user.name)}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href='/account'>
+            Profile
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem disabled>Billing</DropdownMenuItem>
+        <DropdownMenuItem disabled>Subscription</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className='cursor-pointer' onClick={handleLogout}>
+          Logout
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
