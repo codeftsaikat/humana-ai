5c28e1434618cac830e4f643dc83592f58f4db1d
codeftsaikat <soikatdey18@gmail.com>
2024-11-16 12:33:18 +0100

    Refactor authentication configuration and providers in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -8,17 +8,42 @@ import Credentials from "next-auth/providers/credentials"
 import { PrismaAdapter } from '@auth/prisma-adapter'
 
 import { prisma } from "./config/prisma"
+import { Provider } from "next-auth/providers"
+
+const providers: Provider[] = [
+  Github({
+    clientId: process.env.AUTH_GITHUB_ID,
+    clientSecret: process.env.AUTH_GITHUB_SECRET,
+  }),
+  Credentials({
+    credentials: { password: { label: "Password", type: "password" } },
+    authorize(c) {
+      if (c.password !== "password") return null
+      return {
+        id: "test",
+        name: "Test User",
+        email: "test@example.com",
+      }
+    },
+  })
+]
+
+export const providerMaps = providers
+  .map((provider) => {
+    if (typeof provider === 'function') {
+      const providerData = provider()
+      return { id: providerData.id, name: providerData.name }
+    } else {
+      return { id: provider.id, name: provider.name }
+    }
+  })
+  .filter((provider) => provider.id !== 'credentials')
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: {strategy: 'jwt'},
-  providers: [
-    Github({
-      clientId: process.env.AUTH_GITHUB_ID,
-      clientSecret: process.env.AUTH_GITHUB_SECRET,
-    }),
-    Credentials({
-
-    })
-  ],
+  session: { strategy: 'jwt' },
+  providers,
+  pages: {
+    signIn: "/login"
+  }
 })
