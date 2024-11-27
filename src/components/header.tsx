17cb559ced2a6bd3fe7a472e54a2ef230c49e135
codeftsaikat <soikatdey18@gmail.com>
2024-11-27 22:59:11 +0100

    Refactor header component to adjust z-index for improved layering

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,7 +13,7 @@ const Header = async () => {
   const session = await auth() as IAuth
 
   return (
-    <Container className="max-w-5xl left-1/2 -translate-x-1/2 fixed top-4">
+    <Container className="max-w-5xl left-1/2 -translate-x-1/2 fixed z-10 top-4">
       <header className="flex justify-between items-center min-h-16 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border rounded-xl px-4 py-3 shadow-lg">
         <Logo />
         {!session ? <Link href='/login'><Button>Login</Button></Link> : <UserMenu auth={session} />}
