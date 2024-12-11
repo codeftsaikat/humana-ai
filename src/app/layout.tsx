278af99914f1136e419c9c74a7f5faa66f9f359e
codeftsaikat <soikatdey18@gmail.com>
2024-12-11 21:48:57 +0100

    Refactor layout configuration to add 'latin-ext' subset and 'swap' display for improved font rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,8 @@ export const metadata: Metadata = {
 const inter = Inter({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   style: ['normal', 'italic'],
+  subsets: ['latin-ext'],
+  display: 'swap',
 });
 
 export default function RootLayout({
